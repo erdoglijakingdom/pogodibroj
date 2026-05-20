2b7f81345d044e073bef77a258e645b5dd4993ec
erdoglijakingdom <matijajovasevic800@gmail.com>
2026-05-20 14:26:15 +0200

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox txtBroj;
+        private System.Windows.Forms.Button btnProveri;
+        private System.Windows.Forms.Label lblUnesibroj;
+        private System.Windows.Forms.Label lblRezultat;
+        private System.Windows.Forms.Label lblPokusaji;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtBroj = new System.Windows.Forms.TextBox();
+            this.btnProveri = new System.Windows.Forms.Button();
+            this.lblUnesibroj = new System.Windows.Forms.Label();
+            this.lblRezultat = new System.Windows.Forms.Label();
+            this.lblPokusaji = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // txtBroj
+            this.txtBroj.Location = new System.Drawing.Point(220, 100);
+            this.txtBroj.Name = "txtBroj";
+            this.txtBroj.Size = new System.Drawing.Size(80, 20);
+            this.txtBroj.TabIndex = 0;
+
+            // btnProveri
+            this.btnProveri.Location = new System.Drawing.Point(310, 98);
+            this.btnProveri.Name = "btnProveri";
+            this.btnProveri.Size = new System.Drawing.Size(75, 23);
+            this.btnProveri.Text = "Proveri";
+            this.btnProveri.TabIndex = 1;
+            this.btnProveri.Click += new System.EventHandler(this.btnProveri_Click);
+
+            // lblUnesibroj
+            this.lblUnesibroj.AutoSize = true;
+            this.lblUnesibroj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblUnesibroj.Location = new System.Drawing.Point(130, 103);
+            this.lblUnesibroj.Name = "lblUnesibroj";
+            this.lblUnesibroj.Text = "Unesi broj";
+
+            // lblRezultat
+            this.lblRezultat.AutoSize = true;
+            this.lblRezultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblRezultat.Location = new System.Drawing.Point(130, 160);
+            this.lblRezultat.Name = "lblRezultat";
+            this.lblRezultat.Text = "Rezultat";
+
+            // lblPokusaji
+            this.lblPokusaji.AutoSize = true;
+            this.lblPokusaji.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblPokusaji.Location = new System.Drawing.Point(130, 205);
+            this.lblPokusaji.Name = "lblPokusaji";
+            this.lblPokusaji.Text = "Broj pokušaja";
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 311);
+            this.Controls.Add(this.txtBroj);
+            this.Controls.Add(this.btnProveri);
+            this.Controls.Add(this.lblUnesibroj);
+            this.Controls.Add(this.lblRezultat);
+            this.Controls.Add(this.lblPokusaji);
+            this.Name = "Form1";
+            this.Text = "Pogadjanje broja";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private int tajniBroj;
+        private int brojPokusaja;
+        private Random rng = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            NovaIgra();
+        }
+
+        private void NovaIgra()
+        {
+            tajniBroj = rng.Next(1, 101);
+            brojPokusaja = 0;
+            lblRezultat.Text = "Rezultat";
+            lblPokusaji.Text = "Broj pokušaja";
+        }
+
+        private void btnProveri_Click(object sender, EventArgs e)
+        {
+            int pogodak;
+            if (!int.TryParse(txtBroj.Text, out pogodak) || pogodak < 1 || pogodak > 100)
+            {
+                lblRezultat.Text = "Unesite broj od 1 do 100!";
+                return;
+            }
+
+            brojPokusaja++;
+            lblPokusaji.Text = "Broj pokušaja: " + brojPokusaja;
+
+            if (pogodak < tajniBroj)
+            {
+                lblRezultat.Text = "Premali broj!";
+            }
+            else if (pogodak > tajniBroj)
+            {
+                lblRezultat.Text = "Preveliki broj!";
+            }
+            else
+            {
+                lblRezultat.Text = "Tačno! Pogodio si za " + brojPokusaja + " pokušaja!";
+                MessageBox.Show(
+                    "Bravo! Pogodio si broj " + tajniBroj + " za " + brojPokusaja + " pokušaja!",
+                    "Pobeda!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                NovaIgra();
+            }
+
+            txtBroj.Clear();
+            txtBroj.Focus();
+        }
+    }
+}
